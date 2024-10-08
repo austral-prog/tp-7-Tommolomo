4aa4595850b1d9b31bb2141e9dbcadbae635f639
tfarina <134532408+Tommolomo@users.noreply.github.com>
2024-10-07 22:21:18 -0300

    Update loops_and_print.py

--- loops_and_print.py
+++ loops_and_print.py
@@ -1,6 +1,18 @@
-def enumerate_list(list):
-    return list
+def enumerate_list(lista):
+    lista_enu = []
+    indice = 0
+    for string in lista:
+        if string:
+            lista_enu.append(f"{indice}. {string}")
+            indice = indice + 1
+    return lista_enu
 
-
-def enumerate_backwards(list):
-    return list
+def enumerate_backwards(lista2):
+    lista_enu2 = []
+    indice = 0
+    for string in lista2:
+        if string:
+            string_rev = string[::-1]
+            lista_enu2.append(f"{indice}. {string_rev}")
+            indice = indice + 1
+    return lista_enu2
