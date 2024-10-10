5bad5581555f4de6c5b3f5526ad2f4fd1c47732a
tfarina <134532408+Tommolomo@users.noreply.github.com>
2024-10-10 17:23:23 -0300

    Update loops.py

--- loops.py
+++ loops.py
@@ -1,18 +1,33 @@
-def index_of_by_index(word, list, index):
+def index_of_by_index(word, lista, index):
+    for i in range(index, len(lista)):
+        if lista[i] == word:
+            return i
+    return -1
+
+def index_of_empty(lista):
+    for i in range(len(lista)):
+        if lista[i] == "":
+            return i
+    return -1
+
+def index_of(word, lista):
+    for i in range(len(lista)):
+        if lista[i] == word:
+            return i
+    return -1
+
+def put(word, lista):
+    for index, value in enumerate(lista):
+        if value == "":
+            lista[index] = word
+            return index
     return -1
 
 
-def index_of_empty(list):
-    return -1
-
-
-def index_of(word, list):
-    return -1
-
-
-def put(word, list):
-    return -1
-
-
-def remove(word, list):
-    return -1
+def remove(word, lista):
+    count = 0
+    for index, value in enumerate(lista):
+        if value == word:
+            lista[index] = ""
+            count += 1
+    return count
